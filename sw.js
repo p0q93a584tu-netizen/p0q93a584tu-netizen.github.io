61797e2ab31cf16f2eaab9c0b6158c9c04809183
p0q93a584tu-netizen <p0q93a584tu@gmail.com>
2026-03-14 15:33:19 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,75 +1,82 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(installCache());
+  self.skipWaiting();
+  console.log("cache installed");
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async () => {
-      try {
-        if (new URL(event.request.url).pathname === "/") {
-          maybeFlushCache();
-        }
-        let req = await caches.match(event.request);
-        if (req) {
-          let headers = new Headers(req.headers);
-          if (headers.get("Cross-Origin-Embedder-Policy") != "require-corp")
-            headers.append("Cross-Origin-Embedder-Policy", "require-corp");
-          if (headers.get("Cross-Origin-Opener-Policy") != "same-origin")
-            headers.append("Cross-Origin-Opener-Policy", "same-origin");
-
-          return new Response(req.body, {
-            status: req.status,
-            statusText: req.statusText,
-            headers: headers,
-          });
-        }
-        req = await fetch(event.request);
-
-const headers = new Headers(req.headers);
-headers.set("Cross-Origin-Embedder-Policy", "require-corp");
-headers.set("Cross-Origin-Opener-Policy", "same-origin");
-
-return new Response(await req.blob(), {
-  status: req.status,
-  statusText: req.statusText,
-  headers: headers
+  event.respondWith(handleRequest(event.request));
 });
-      } catch (e) {
-        console.log("error", e);
-        return new Response("Worker error", {
-          status: 500,
-          statusText: "Network error",
-        });
+
+async function handleRequest(request) {
+  try {
+    if (new URL(request.url).pathname === "/") {
+      maybeFlushCache();
+    }
+
+    const cache = await caches.open("v1");
+    let response = await cache.match(request);
+
+    if (!response) {
+      response = await fetch(request);
+
+      if (request.method === "GET" && response.ok) {
+        cache.put(request, response.clone());
       }
-    })(),
-  );
-});
+    }
+
+    const headers = new Headers(response.headers);
+    headers.set("Cross-Origin-Embedder-Policy", "require-corp");
+    headers.set("Cross-Origin-Opener-Policy", "same-origin");
+
+    return new Response(await response.blob(), {
+      status: response.status,
+      statusText: response.statusText,
+      headers: headers,
+    });
+
+  } catch (e) {
+    console.log("error", e);
+    return new Response("Worker error", {
+      status: 500,
+      statusText: "Network error",
+    });
+  }
+}
 
 async function installCache() {
   const cache = await caches.open("v1");
-  const boot = await fetch("_framework/blazor.boot.json");
+
+  const boot = await fetch("/web/_framework/blazor.boot.json");
   const bootjson = await boot.json();
-  let resources = [
+
+  const resources = [
     "/",
+    "/web/",
     "/MILESTONE",
-    "/_framework/blazor.boot.json",
-    "/app.ico",
-    "/backdrop.png",
-    "/AndyBold.ttf",
-    "/assets/index.js",
-    "/assets/index.css",
+    "/web/_framework/blazor.boot.json",
+    "/web/app.ico",
+    "/web/backdrop.png",
+    "/web/AndyBold.ttf",
+    "/web/assets/index.js",
+    "/web/assets/index.css",
     ...Object.keys(bootjson.resources.fingerprinting).map(
-      (r) => "_framework/" + r,
+      (r) => "/web/_framework/" + r
     ),
   ];
+
   await cache.addAll(resources);
 }
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(installCache());
-  console.log("cache installed");
-});
 
 async function maybeFlushCache() {
   const cachedmilestone = await caches.match("/MILESTONE");
   const response = await fetch("/MILESTONE");
   const milestone = await response.text();
+
   if (cachedmilestone) {
     const cachedmilestoneText = await cachedmilestone.text();
     if (cachedmilestoneText === milestone) {
@@ -83,7 +90,3 @@ async function maybeFlushCache() {
     return Promise.all(cacheNames.map((name) => caches.delete(name)));
   });
 }
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(maybeFlushCache());
-});
